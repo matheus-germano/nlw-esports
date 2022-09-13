9ffe92eee99b8c83c8c49dfd1713981758ace457
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-13 19:17:59 -0300

    style: heading done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
 import './styles/main.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div className="w-8 h-8"></div>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logo} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.</h1>
+    </div>
   )
 }
 
