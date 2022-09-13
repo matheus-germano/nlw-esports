057577f8400ef8c00bb9bb533c6fe49376ab1409
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-13 11:16:21 -0300

    build: tailwind css added and seted

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-function App() {
+import './styles/main.css';
 
+function App() {
   return (
-    <h1>Hello world</h1>
+    <div className="w-8 h-8 bg-black dark:bg-slate-500"></div>
   )
 }
 
