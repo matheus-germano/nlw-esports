3e35fc2c03852f006aaf7cdbfa33ec6fdf4ce3fd
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-14 17:15:59 -0300

    Initial endpoints structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,22 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', () => console.log("ola"));
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.json([]);
+});
+
+app.get('games/:id/ads', (request, response) => {
+  const gameId = request.params.id;
+  return response.json([]);
+});
+
+app.get('ads/:id/discord', (request, response) => {
+  const adId = request.params.id;
+  return response.json([]);
+});
 
 app.listen(3333);
