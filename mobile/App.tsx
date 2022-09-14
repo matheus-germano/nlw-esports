e86c6b9cf97a53852563556f2c12e18cd6099875
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-14 10:10:02 -0300

    style: background image

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import { Background } from './src/components/Background';
+
+export default function App() {
+  return (
+    <Background>
+
+    </Background>
+  );
+}
