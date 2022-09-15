048827057e6b10ea13dfe4b66e3b7e80cdbb0c2b
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-14 22:35:17 -0300

    feat: Game and Ad table created

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,12 +10,12 @@ app.post('/ads', (request, response) => {
   return response.json([]);
 });
 
-app.get('games/:id/ads', (request, response) => {
+app.get('/games/:id/ads', (request, response) => {
   const gameId = request.params.id;
   return response.json([]);
 });
 
-app.get('ads/:id/discord', (request, response) => {
+app.get('/ads/:id/discord', (request, response) => {
   const adId = request.params.id;
   return response.json([]);
 });
