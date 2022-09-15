28ef267ceaf7dd0767f8f0ea775980b10b9219c4
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-15 11:20:43 -0300

    refactor: changing basic view to SafeAreaView

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-
-import { View, Image, FlatList } from 'react-native';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Heading } from '../../components/Heading';
 import { GameCard, GameCardProps } from '../../components/GameCard';
@@ -19,7 +19,7 @@ export function Home() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image
         source={logoImg}
         style={styles.logo}
@@ -42,6 +42,6 @@ export function Home() {
         showsHorizontalScrollIndicator={false}
         horizontal
       />
-    </View>
+    </SafeAreaView>
   );
 }
